e9424354dd5e874d822f271a5b22b2c38bb7fe3c
ereng <aereng@gmail.com>
2015-03-02 16:27:02 +0300

    kap migration

---
+++ scripts/kap_migration_script.sql
@@ -0,0 +1,369 @@
+-- USER MIGRATIONS
+ 
+-- Script to migrate users data from old-blis to new blis
+-- 0 warnings
+-- The passwords from old - Blis are not functional in the new
+
+insert into iblis.users
+  (id, username, password, name, email)
+select distinct user.user_id, username, password, 
+if (actualname = '', username, actualname  ) as username, 
+  if (email is NULL, '', email) as email
+from blis_revamp_prod.user
+left join blis_302.test 
+on (test.user_id=user.user_id)
+where test.user_id=user.user_id
+group by user.user_id;
+
+-- ROLES MIGRATIONS
+
+INSERT INTO iblis.roles (id, name, description, created_at, updated_at) VALUES
+(1, 'Superadmin', NULL, '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(2, 'Technologist', NULL, '2015-02-27 13:10:09', '2015-02-27 13:10:09'),
+(3, 'Receptionist', NULL, '2015-02-27 13:10:09', '2015-02-27 13:10:09');
+
+INSERT INTO iblis.assigned_roles (id, user_id, role_id) VALUES
+(1, 1, 1);
+
+-- PERMISSIONS MIGRATIONS
+
+INSERT INTO iblis.permissions (id, name, display_name, created_at, updated_at) VALUES
+(1, 'view_names', 'Can view patient names', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(2, 'manage_patients', 'Can add patients', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(3, 'receive_external_test', 'Can receive test requests', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(4, 'request_test', 'Can request new test', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(5, 'accept_test_specimen', 'Can accept test specimen', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(6, 'reject_test_specimen', 'Can reject test specimen', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(7, 'change_test_specimen', 'Can change test specimen', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(8, 'start_test', 'Can start tests', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(9, 'enter_test_results', 'Can enter tests results', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(10, 'edit_test_results', 'Can edit test results', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(11, 'verify_test_results', 'Can verify test results', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(12, 'send_results_to_external_system', 'Can send test results to external systems', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(13, 'refer_specimens', 'Can refer specimens', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(14, 'manage_users', 'Can manage users', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(15, 'manage_test_catalog', 'Can manage test catalog', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(16, 'manage_lab_configurations', 'Can manage lab configurations', '2015-02-27 13:10:08', '2015-02-27 13:10:08'),
+(17, 'view_reports', 'Can view reports', '2015-02-27 13:10:08', '2015-02-27 13:10:08');
+
+
+INSERT INTO iblis.permission_role (id, permission_id, role_id) VALUES
+(1, 1, 1),
+(2, 2, 1),
+(3, 3, 1),
+(4, 4, 1),
+(5, 5, 1),
+(6, 6, 1),
+(7, 7, 1),
+(8, 8, 1),
+(9, 9, 1),
+(10, 10, 1),
+(11, 11, 1),
+(12, 12, 1),
+(13, 13, 1),
+(14, 14, 1),
+(15, 15, 1),
+(16, 16, 1),
+(17, 17, 1);
+
+
+-- PATIENTS MIGRATIONS
+
+-- Script to migrate patients data from old-blis to new blis
+-- 0 warnings
+-- Some records have no calculatable date of birth, I have put the DOB as the timestamp
+
+
+insert into iblis.patients 
+  (patient_number, name, dob, gender, external_patient_number, created_at)
+ 
+select  patient_id, name, 
+  (case WHEN dob = '0000-00-00'
+  THEN CONVERT( 
+    date_sub(DATE_FORMAT(ts,  '%Y-%m-%d'), INTERVAL age year)
+    USING latin1 )
+  WHEN dob IS NULL 
+  THEN  partial_dob 
+ELSE dob 
+END) as dob,
+ 
+if(sex = 'M', 0, 1) as sex, surr_id, ts  from blis_302.patient;
+
+
+
+-- SPECIMEN TYPES MIGRATIONS
+
+-- Scripts to migrate specimen types
+-- 0 errors 0 warnings
+-- No anomalies
+
+insert into iblis.specimen_types (id, name, description, created_at) 
+  select specimen_type_id, name, description, ts from blis_302.specimen_type;
+
+-- REJECTION REASONS MIGRATIONS
+
+insert into iblis.rejection_reasons (id, reason) values
+(1, 'Poorly labelled'),
+(2, 'Over saturation'),
+(3, 'Insufficient Sample'),
+(4, 'Scattered'),
+(5, 'Clotted Blood'),
+(6, 'Two layered spots'),
+(7, 'Serum rings'),
+(8, 'Scratched'),
+(9, 'Haemolysis'),
+(10, 'Spots that cannot elute'),
+(11, 'Leaking'),
+(12, 'Broken Sample Container'),
+(13, 'Mismatched sample and form labelling'),
+(14, 'Missing Labels on container and tracking form'),
+(15, 'Empty Container'),
+(16, 'Samples without tracking forms'),
+(17, 'Poor transport'),
+(18, 'Lipaemic'),
+(19, 'Wrong container/Anticoagulant'),
+(20, 'Request form without samples'),
+(21, 'Missing collection date on specimen / request form.'),
+(22, 'Name and signature of requester missing'),
+(23, 'Mismatched information on request form and specimen container.'),
+(24, 'Request form contaminated with specimen'),
+(25, 'Duplicate specimen received'),
+(26, 'Delay between specimen collection and arrival in the laboratory'),
+(27, 'Inappropriate specimen packing'),
+(28, 'Inappropriate specimen for the test'),
+(29, 'Inappropriate test for the clinical condition'),
+(30, 'No Label'),
+(31, 'Leaking'),
+(32, 'No Sample in the Container'),
+(33, 'No Request Form'),
+(34, 'Missing Information Required');
+
+
+
+-- SPECIMEN STATUSES MIGRATION
+
+-- data is from the laravel seed
+-- no errors
+
+insert into iblis.specimen_statuses (id, name)
+  values
+    (1, "specimen-not-collected" ),
+    (2, "specimen-accepted"),
+    (3, "specimen-rejected");
+
+
+-- TEST CATEGORY MIGRATIONS
+ 
+-- Script to migrate test categories from old-blis to new blis
+-- 0 warnings
+insert into iblis.test_categories
+  (id, name, description, created_at)
+select test_category_id, name, description, ts 
+from blis_302.test_category;
+ 
+
+-- MEASURE TYPE MIGRATIONS
+ 
+-- Script to migrate measure types from old-blis to new blis
+-- 0 warnings
+insert into iblis.measure_types
+  (id, name) 
+values
+  ('1', 'Numeric Range'),
+  ('2', 'Alphanumeric Values'),
+  ('3', 'Autocomplete'),
+  ('4', 'Free Text');
+
+-- MEASURE MIGRATIONS
+ 
+-- Script to migrate measures from old-blis to new blis
+-- converting auto complete to alphanumeric
+-- 0 warnings
+insert into iblis.measures
+  (id, measure_type_id, name, unit, description, created_at)
+select measure_id,
+(case 
+  WHEN measure_range = '$freetext$$' THEN '4' 
+  WHEN measure_range = ':' THEN '1' 
+  ELSE '2' 
+END) as measure_type_id,
+name, 
+if (unit is NULL, '', unit) as unit,
+description, ts
+from blis_302.measure;
+
+-- Migration for alphanumeric and 'autocomplete' measure_ranges
+
+CREATE TABLE iblis.tmp_ranges(id int not null primary key AUTO_INCREMENT, measure_id int, alphanumeric varchar(500));
+INSERT into iblis.tmp_ranges(measure_id, alphanumeric)
+  select measure_id, measure_range
+  from blis_302.measure
+  where measure_range != ':' and measure_range != '$freetext$$';
+
+UPDATE iblis.tmp_ranges SET alphanumeric = REPLACE (alphanumeric, '_', '/');
+
+
+DELIMITER $$
+CREATE FUNCTION iblis.strSplit(x VARCHAR(65000), delim VARCHAR(12), pos INTEGER) 
+RETURNS VARCHAR(65000)
+BEGIN
+  DECLARE output VARCHAR(65000);
+  SET output = REPLACE(SUBSTRING(SUBSTRING_INDEX(x, delim, pos)
+                 , LENGTH(SUBSTRING_INDEX(x, delim, pos - 1)) + 1)
+                 , delim
+                 , '');
+  IF output = '' THEN SET output = null; END IF;
+  RETURN output;
+END $$
+ 
+CREATE PROCEDURE iblis.MeasureRanges2Alphanumeric()
+BEGIN
+  DECLARE i INTEGER;
+ 
+  SET i = 1;
+  REPEAT
+    INSERT INTO iblis.measure_ranges (measure_id, alphanumeric)
+      SELECT measure_id, strSplit(alphanumeric, '/', i) FROM iblis.tmp_ranges
+      WHERE strSplit(alphanumeric, '/', i) IS NOT NULL ORDER BY measure_id ASC;
+    SET i = i + 1;
+    UNTIL ROW_COUNT() = 0
+  END REPEAT;
+END $$
+DELIMITER ;
+-- Call the procedure
+CALL iblis.MeasureRanges2Alphanumeric;
+
+-- TEST TYPE MIGRATIONS
+ 
+-- Script to migrate test types from old-blis to new blis
+-- 0 warnings
+insert into iblis.test_types
+  (id, name, description, test_category_id, targetTAT, prevalence_threshold, created_at)
+select test_type_id, name, description, test_category_id, target_tat, prevalence_threshold, ts
+from blis_302.test_type where disabled = 0;
+ 
+-- TESTTYPE MASURE MIGRATIONS
+ 
+-- Script to migrate test type measures types from old-blis to new blis
+-- 0 warnings
+insert into iblis.testtype_measures
+  ( test_type_id, measure_id)
+select ttm.test_type_id, ttm.measure_id
+from blis_302.test_type_measure ttm
+inner join test_type tt
+  on tt.test_type_id=ttm.test_type_id
+join measure m
+  on m.measure_id=ttm.measure_id;
+
+
+
+INSERT INTO iblis.test_phases (id, name) VALUES (1, "Pre-Analytical"),(2, "Analytical"),(3, "Post-Analytical");
+
+INSERT INTO iblis.test_statuses(id, name, test_phase_id) VALUES (1, "not-received", 1),
+  (2, "pending", 1),(3, "started", 2),(4, "completed", 3),(5, "verified", 3);
+-- SPECIMEN MIGRATION SCRIPT
+
+-- depends on referrals-script, specimen-types, specimen_statuses script 
+
+insert into iblis.specimens 
+  (id, specimen_type_id, specimen_status_id,  accepted_by, rejected_by,
+    reject_explained_to, time_accepted, time_rejected )
+    select specimen_id, specimen_type_id, 
+    (CASE WHEN status_code_id = 0 THEN 2
+        WHEN status_code_id = 1 THEN 2
+        WHEN status_code_id = 6 THEN 3
+        WHEN status_code_id = 8 THEN 1
+    END) AS status_code_id,
+    s.user_id, 
+    if (s.aux_id = '', 0 , s.aux_id) as aux_id, 
+    s.referred_to_name,
+    ts_collected, 
+    if(s.specimen_id = 6, ts, null) as ts
+  from blis_302.specimen s;
+
+
+-- EXTERNAL LAB REQUEST MIGRATIONS
+ 
+-- Script to migrate eternal lab requests from old-blis to new blis
+-- To speed up the migration the patientVisitNumber column is made an index first
+-- 0 errors, 0 warnings
+
+insert into iblis.external_dump 
+  ( id,
+    lab_no,
+    parent_lab_no,
+    test_id,
+    requesting_clinician,
+    investigation,
+    provisional_diagnosis,
+    request_date,
+    order_stage,
+    result,
+    result_returned,
+    patient_id,
+    full_name,
+    dob,
+    gender,
+    address,
+    postal_code,
+    phone_number,
+    city,
+    cost,
+    receipt_number,
+    receipt_type,
+    waiver_no,
+    system_id)
+select id,
+  labNo,
+  parentLabNo,
+  t.test_id,
+  requestingClinician,
+  investigation,
+  provisionalDiagnosis,
+  requestDate,
+  orderStage,
+  extr.result,
+  result_returned,
+  patient_id,
+  full_name,
+  dateOfBirth,
+  gender,
+  address,
+  postalCode,
+  phoneNumber,
+  city,
+  cost,
+  receiptNumber,
+  receiptType,
+  waiverNo,
+  system_id
+from blis_revamp_prod.external_lab_request extr
+left join blis_302.test t
+on (extr.labNo = t.external_lab_no)
+group by id;
+
+
+-- Inserting external user_id's for sending back results 
+
+-- 0 warnings
+
+insert into iblis.external_users (internal_user_id, external_user_id)
+  select user_id, emr_user_id from blis_revamp_prod.user s inner join iblis.users u on u.id = s.user_id
+    order by user_id asc;
+
+
+-- VISITS migration scripts
+
+-- 0 warnings 0 errors
+
+drop table if exists iblis.tmp_visits;
+
+CREATE TABLE iblis.tmp_visits SELECT group_concat(t.test_id) test_ids, s.patient_id, 
+IFNULL(x.orderStage,'op') AS order_stage, t.ts 
+FROM blis_302.test t INNER JOIN blis_302.specimen s ON t.specimen_id = s.specimen_id 
+LEFT JOIN blis_revamp_prod.external_lab_request x ON t.external_lab_no = x.labNo 
+GROUP BY patient_id, substr(t.ts, 1, 10) ORDER BY t.test_id;  
+
+INSERT INTO iblis.visits (patient_id, visit_type, created_at) 
+SELECT patient_id, 
+IF(order_stage='op','Out-Patient', 'In-Patient')ords, ts created_at FROM iblis.tmp_visits;
